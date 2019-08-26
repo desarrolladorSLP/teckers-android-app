8bee825768ef5b715f5a979a81538c0ce19ce0d4
anaimhunter <anaimhunter@hotmail.com>
2019-08-26 16:44:44 -0500

    Menu bar files and activityhome with design

---
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package org.desarrolladorslp.teckersapp.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
