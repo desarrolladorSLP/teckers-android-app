46dc01350d0e756d50c2f673f64ec1652e650a2b
anaimhunter <anaimhunter@hotmail.com>
2019-09-03 16:00:52 -0500

    Example LiveData

--- app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/MessageViewModel.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/MessageViewModel.kt
@@ -6,5 +6,8 @@ import androidx.lifecycle.ViewModel
 
 class MessageViewModel : ViewModel() {
 
-
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
 }
