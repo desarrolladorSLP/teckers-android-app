9b978f0d66471de5fd4f5bbc15db020576993184
anaimhunter <anaimhunter@hotmail.com>
2019-09-05 14:44:25 -0500

    Section page adapter

--- app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/SectionsPagerAdapter.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/SectionsPagerAdapter.kt
@@ -1,10 +1,10 @@
-package com.example.tabbed.ui.main
+package org.desarrolladorslp.teckersapp.ui.messages
 
 import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.example.tabbed.R
+import org.desarrolladorslp.teckersapp.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
