4d28797d5e866070986a328af061cdb9b230b1df
anaimhunter <anaimhunter@hotmail.com>
2019-09-05 14:44:00 -0500

    Placeholder

--- app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/PlaceholderFragment.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/PlaceholderFragment.kt
@@ -1,4 +1,4 @@
-package com.example.tabbed.ui.main
+package org.desarrolladorslp.teckersapp.ui.messages
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,18 +8,18 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.tabbed.R
+import org.desarrolladorslp.teckersapp.R
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class PlaceholderFragment : Fragment() {
 
-    private lateinit var pageViewModel: PageViewModel
+        private lateinit var pageViewModel: MessageViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
+        pageViewModel = ViewModelProviders.of(this).get(MessageViewModel::class.java).apply {
             setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
         }
     }
@@ -28,7 +28,7 @@ class PlaceholderFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_main, container, false)
+        val root = inflater.inflate(R.layout.fragment_messages, container, false)
         val textView: TextView = root.findViewById(R.id.section_label)
         pageViewModel.text.observe(this, Observer<String> {
             textView.text = it
