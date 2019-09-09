fcb2f9050e37d047163bef05fb7494c1fc7cdeac
anaimhunter <anaimhunter@hotmail.com>
2019-09-09 18:03:53 -0500

    MessageAdapter and Holder for the recyclerview

--- app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/MessageAdapter.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/MessageAdapter.kt
@@ -1,4 +1,59 @@
 package org.desarrolladorslp.teckersapp.ui.messages
 
-class MessageAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.message_item.view.*
+import org.desarrolladorslp.teckersapp.R
+import org.desarrolladorslp.teckersapp.model.MessageHeader
+
+
+data class MessageAdapter(private val messagesHeader: ArrayList<MessageHeader>) : RecyclerView.Adapter<MessageAdapter.MessageHeaderHolder>()  {
+
+    override fun getItemCount() = messagesHeader.size
+
+    override fun onBindViewHolder(holder: MessageHeaderHolder, position: Int) {
+        val itemMessageHeader = messagesHeader[position]
+        holder.bindMessageHeader(itemMessageHeader)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageHeaderHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.message_item, parent, false) as ImageView
+
+        return MessageHeaderHolder(view)
+    }
+    class MessageHeaderHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+
+        private var view: View = v
+        private var messageheader: MessageHeader? = null
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            Log.d("RecyclerView", "CLICK!")
+        }
+
+        fun bindMessageHeader(messageheader: MessageHeader) {
+            this.messageheader = messageheader
+            Picasso.get().load(messageheader.senderImage).into(view.imageView)
+            view.sender.text = messageheader.sender
+            view.subject.text = messageheader.subject
+            view.timestamp.text = messageheader.timestamp
+        }
+
+        companion object {
+            private val senderImage_Key = "SENDERIMAGE"
+        }
+
+    }
+
 }
