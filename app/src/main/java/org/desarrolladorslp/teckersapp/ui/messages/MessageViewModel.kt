b2fd3c5fc37f4c985c51e4cd8be7510661ff09ea
anaimhunter <anaimhunter@hotmail.com>
2019-09-16 14:07:27 -0500

    Changed data of the messages

--- app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/MessageViewModel.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/MessageViewModel.kt
@@ -47,8 +47,16 @@ class MessageViewModel : ViewModel() {
             "https://st-listas.20minutos.es/images/2016-01/406254/4881186_640px.jpg","Bye",
             "19-08-2019")
 
+        val message3= MessageHeader("Ariana Grande",
+            "https://los40es00.epimg.net/los40/imagenes/2019/08/11/actualidad/1565512687_081489_1565512863_noticia_normal.jpg",
+            "Hi",
+            "19-07-2019")
+        val message4= MessageHeader("Miley Cyrus",
+            "https://www.diariogol.com/uploads/s1/64/82/48/6/miley-cyrus_16_643x397.jpeg","Bye",
+            "19-08-2019")
+
         val messagesHighPriority = arrayListOf(message1,message2)
-        val messagesLowPriority = arrayListOf(message1,message2)
+        val messagesLowPriority = arrayListOf(message3,message4)
         val inbox=Inbox(messagesHighPriority,messagesLowPriority)
 
         return inbox
