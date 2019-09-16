9feefc29b92c5ea216a0dc0ddd7e820a6aec6d83
anaimhunter <anaimhunter@hotmail.com>
2019-09-16 11:42:14 -0500

    Messages Fragment change for pages view

--- app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/MessageFragment.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/MessageFragment.kt
@@ -8,25 +8,34 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.content_messages.*
 import org.desarrolladorslp.teckersapp.R
 import org.desarrolladorslp.teckersapp.model.Inbox
 import org.desarrolladorslp.teckersapp.model.MessageHeader
 
 
-
-
 class MessageFragment : Fragment() {
 
     private lateinit var messageViewModel: MessageViewModel
-    private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: MessageAdapter
-    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var viewPager: ViewPager
+    private lateinit var sectionsPagerAdapter: SectionsPagerAdapter
+    private lateinit var  tabs: TabLayout
+    private lateinit var myContext: FragmentActivity
+    private  lateinit var  managerFragment: FragmentManager
+
+    override fun onAttach(activity: Activity) {
+        myContext = activity as FragmentActivity
+        super.onAttach(activity)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,12 +45,14 @@ class MessageFragment : Fragment() {
         messageViewModel =
             ViewModelProviders.of(this).get(MessageViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_messages, container, false)
-        viewManager = LinearLayoutManager(context)
+        managerFragment = myContext.getSupportFragmentManager()
+        sectionsPagerAdapter = SectionsPagerAdapter(context!!,managerFragment)
         viewAdapter = MessageAdapter(messageViewModel.totalMessages())
-        recyclerView= root.findViewById<RecyclerView>(R.id.messagesList).apply{
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
+        viewPager = root.findViewById<ViewPager>(R.id.view_pager).apply{
+            adapter= sectionsPagerAdapter
+        }
+        tabs= root.findViewById<TabLayout>(R.id.tabs).apply{
+            setupWithViewPager(viewPager)
         }
         return root
     }
