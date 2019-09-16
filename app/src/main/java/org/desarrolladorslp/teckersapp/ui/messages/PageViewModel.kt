087dd0c6bfc8b100a2f060c3a19973f8d6be9647
anaimhunter <anaimhunter@hotmail.com>
2019-09-16 13:45:38 -0500

    Add the inbox and messages

--- app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/PageViewModel.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/messages/PageViewModel.kt
@@ -4,14 +4,26 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import org.desarrolladorslp.teckersapp.model.Inbox
+import org.desarrolladorslp.teckersapp.model.MessageHeader
 
 class PageViewModel : ViewModel() {
-    private val _index = MutableLiveData<Int>()
-    val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
+    private var priorityIndex =1
+    private lateinit var inbox: Inbox
+
+    fun getmessages() :ArrayList<MessageHeader> {
+        if(priorityIndex==2)
+        {
+            return inbox.lowPriority
+        }
+        return  inbox.highPriority
     }
 
+    fun setInbox(inbox:Inbox)
+    {
+        this.inbox = inbox
+    }
     fun setIndex(index: Int) {
-        _index.value = index
+        priorityIndex = index
     }
 }
