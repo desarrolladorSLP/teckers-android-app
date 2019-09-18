676e674499dcd05c111eb0a563e6cad531475c02
anaimhunter <anaimhunter@hotmail.com>
2019-09-17 22:48:06 -0500

    App credentials added

--- app/src/main/java/org/desarrolladorslp/teckersapp/service/AppCredentialsInterceptor.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/service/AppCredentialsInterceptor.kt
@@ -2,11 +2,15 @@ package org.desarrolladorslp.teckersapp.service
 
 import okhttp3.Interceptor
 import okhttp3.Response
+import org.desarrolladorslp.teckersapp.BuildConfig
 
 class AppCredentialsInterceptor() : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
 
+        val appCredentials : String = BuildConfig.user +":"+ BuildConfig.password
+        open class Base64
+        val decode: ByteArray = android.util.Base64.decode(appCredentials,0)
         var request = chain.request()
         val headers =
             request.headers().newBuilder()
