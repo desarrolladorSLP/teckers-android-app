c6f30693de8b88f505f8f120f4af208d125456ec
anaimhunter <anaimhunter@hotmail.com>
2019-09-20 14:11:07 -0500

    Base 64 app credentials

--- app/src/main/java/org/desarrolladorslp/teckersapp/service/AppCredentialsInterceptor.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/service/AppCredentialsInterceptor.kt
@@ -1,20 +1,23 @@
 package org.desarrolladorslp.teckersapp.service
 
+import android.util.Base64
 import okhttp3.Interceptor
 import okhttp3.Response
 import org.desarrolladorslp.teckersapp.BuildConfig
+
 
 class AppCredentialsInterceptor() : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
 
-        val appCredentials : String = BuildConfig.user +":"+ BuildConfig.password
-        open class Base64
-        val decode: ByteArray = android.util.Base64.decode(appCredentials,0)
+        val appCredentials  = BuildConfig.user +":"+ BuildConfig.password
+        val charset = Charsets.UTF_8
+        val byteArray = appCredentials.toByteArray(charset)
+        val base64 = Base64.encodeToString(byteArray, Base64.DEFAULT)
         var request = chain.request()
         val headers =
             request.headers().newBuilder()
-                .add("Authorization", "Basic QW5kcm9pZEFwcDp2bGV3MzV4OQ==").build()
+                .add("Authorization", "Basic " +base64).build()
 
         request = request.newBuilder().headers(headers).build()
 
