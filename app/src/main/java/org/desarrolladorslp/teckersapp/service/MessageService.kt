7a96e449ad5dedf87f42df24f3ca736524aefce8
anaimhunter <anaimhunter@hotmail.com>
2019-09-24 15:49:08 -0500

    Messages Service

--- app/src/main/java/org/desarrolladorslp/teckersapp/service/MessageService.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/service/MessageService.kt
@@ -1,4 +1,16 @@
 package org.desarrolladorslp.teckersapp.service
 
+import org.desarrolladorslp.teckersapp.model.Inbox
+import org.desarrolladorslp.teckersapp.model.MessageHeader
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+
 interface MessageService {
+
+    @FormUrlEncoded
+    @GET("/api/messages")
+    fun getMessages(): Call<Inbox>
 }
+
