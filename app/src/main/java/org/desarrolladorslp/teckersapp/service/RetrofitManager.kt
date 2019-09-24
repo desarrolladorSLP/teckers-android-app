81582ec6265f60a2d8c3e4ca6ad0d0527d6eb813
anaimhunter <anaimhunter@hotmail.com>
2019-09-24 16:20:13 -0500

    RetrofitManager class rename

---
+++ app/src/main/java/org/desarrolladorslp/teckersapp/service/RetrofitManager.kt
@@ -0,0 +1,32 @@
+package org.desarrolladorslp.teckersapp.service
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitManager {
+
+    companion object {
+        private var authInstance: Retrofit? = null
+
+        fun instance(): Retrofit? {
+
+            if (authInstance == null) {
+
+                val client = OkHttpClient.Builder()
+                    .addInterceptor(AppCredentialsInterceptor())
+                    .build()
+
+                authInstance = Retrofit.Builder()
+                    .baseUrl("https://teckers-backend.herokuapp.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build()
+            }
+
+            return authInstance
+        }
+
+    }
+}
+
