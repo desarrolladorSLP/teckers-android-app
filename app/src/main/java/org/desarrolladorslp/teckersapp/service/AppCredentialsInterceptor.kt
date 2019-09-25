f6d6dbd77211e65238b4191dc28486a32f36e484
anaimhunter <anaimhunter@hotmail.com>
2019-09-25 13:07:22 -0500

    Concatenate the app credentials

--- app/src/main/java/org/desarrolladorslp/teckersapp/service/AppCredentialsInterceptor.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/service/AppCredentialsInterceptor.kt
@@ -17,7 +17,7 @@ class AppCredentialsInterceptor() : Interceptor {
         var request = chain.request()
         val headers =
             request.headers().newBuilder()
-                .add("Authorization", "Basic " +base64).build()
+                .add("Authorization", "Basic $base64").build()
 
         request = request.newBuilder().headers(headers).build()
 
