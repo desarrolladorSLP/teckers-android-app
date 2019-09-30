a7821ae535a6f8fbb1ae9ee7c9b0f3bfb5bf8222
anaimhunter <anaimhunter@hotmail.com>
2019-09-29 22:51:09 -0500

    ApiEndpoint intercept method override

--- app/src/main/java/org/desarrolladorslp/teckersapp/service/APIEndpoint.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/service/APIEndpoint.kt
@@ -1,14 +1,18 @@
 package org.desarrolladorslp.teckersapp.service
 
+import androidx.core.content.ContextCompat.startActivity
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import org.desarrolladorslp.teckersapp.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 class APIEndpoint {
 
     companion object {
-        private val interceptor = TokenInterceptor()
+        private var interceptor = TokenInterceptor()
         private var apiInstance: Retrofit? = null
         private var baseUrl= BuildConfig.BaseUrl
 
@@ -16,7 +20,19 @@ class APIEndpoint {
             if (apiInstance == null) {
 
                 var client = OkHttpClient.Builder()
-                    .addInterceptor(interceptor)
+                    .addInterceptor(object : TokenInterceptor() {
+                        @Throws(IOException::class)
+                        override fun intercept(chain: Interceptor.Chain): Response {
+                            var request = chain.request()
+                            var response = chain.proceed(request)
+                            if(response.code() == 500)
+                            {
+                                //muestra el toast en el activity
+                                return response
+                            }
+                            return response
+                        }
+                    })
                     .build()
 
                 apiInstance = Retrofit.Builder()
