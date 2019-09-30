f73c8a0def0ca799944e1bbe5230516133bd3c1e
anaimhunter <anaimhunter@hotmail.com>
2019-09-29 20:51:09 -0500

    BaseUrl added by variable

--- app/src/main/java/org/desarrolladorslp/teckersapp/service/APIEndpoint.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/service/APIEndpoint.kt
@@ -1,6 +1,7 @@
 package org.desarrolladorslp.teckersapp.service
 
 import okhttp3.OkHttpClient
+import org.desarrolladorslp.teckersapp.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,6 +10,7 @@ class APIEndpoint {
     companion object {
         private val interceptor = TokenInterceptor()
         private var apiInstance: Retrofit? = null
+        private var baseUrl= BuildConfig.BaseUrl
 
         fun instance(): Retrofit? {
             if (apiInstance == null) {
@@ -19,7 +21,7 @@ class APIEndpoint {
 
                 apiInstance = Retrofit.Builder()
                     .client(client)
-                    .baseUrl("https://teckers-backend.herokuapp.com/")
+                    .baseUrl(baseUrl)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
             }
