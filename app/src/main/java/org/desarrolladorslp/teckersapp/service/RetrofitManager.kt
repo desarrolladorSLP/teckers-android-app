71feb6942f01f74bcbb49c7b83b65a6e44cc3c0f
anaimhunter <anaimhunter@hotmail.com>
2019-09-29 22:51:36 -0500

    Retrofit manager intercept method override

--- app/src/main/java/org/desarrolladorslp/teckersapp/service/RetrofitManager.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/service/RetrofitManager.kt
@@ -1,9 +1,12 @@
 package org.desarrolladorslp.teckersapp.service
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import org.desarrolladorslp.teckersapp.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 class RetrofitManager {
 
@@ -15,7 +18,19 @@ class RetrofitManager {
             if (authInstance == null) {
 
                 val client = OkHttpClient.Builder()
-                    .addInterceptor(AppCredentialsInterceptor())
+                    .addInterceptor(object : AppCredentialsInterceptor() {
+                        @Throws(IOException::class)
+                        override fun intercept(chain: Interceptor.Chain): Response {
+                            var request = chain.request()
+                            var response = chain.proceed(request)
+                            if(response.code() == 500)
+                            {
+                                //muestra el toast en el activity
+                                return response
+                            }
+                            return response
+                        }
+                    })
                     .build()
 
                 authInstance = Retrofit.Builder()
