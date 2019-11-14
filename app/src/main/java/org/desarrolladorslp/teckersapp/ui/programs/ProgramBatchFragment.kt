53e1ed1554c1a7e8ebf7ed08573303d25986ee08
anaimhunter <anaimhunter@hotmail.com>
2019-11-13 20:23:42 -0600

    Design change

--- app/src/main/java/org/desarrolladorslp/teckersapp/ui/programs/ProgramBatchFragment.kt
+++ app/src/main/java/org/desarrolladorslp/teckersapp/ui/programs/ProgramBatchFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -15,6 +17,7 @@ import org.desarrolladorslp.teckersapp.R
 import org.desarrolladorslp.teckersapp.model.Batch
 import org.desarrolladorslp.teckersapp.model.Program
 import org.desarrolladorslp.teckersapp.ui.batches.BatchViewModel
+import org.desarrolladorslp.teckersapp.ui.teckers.TeckerFragment
 
 class ProgramBatchFragment: Fragment() {
 
@@ -22,8 +25,10 @@ class ProgramBatchFragment: Fragment() {
     lateinit var programsViewModel:ProgramViewModel
     lateinit var batchSpinner: Spinner
     lateinit var batchesViewModel: BatchViewModel
+    lateinit var approvalButton :Button
     var selectedProgram: Program? = null
     var selectedBatch: Batch?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,15 +42,22 @@ class ProgramBatchFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val  root= inflater.inflate(R.layout.fragment_programs_batches, container, false)
         programSpinner = root.findViewById(R.id.spinner_programs) as Spinner
         batchSpinner = root.findViewById(R.id.spinner_batches) as Spinner
+       // approvalButton  = root.findViewById(R.id.btn_approval)as Button
         programsViewModel._programs.observe(activity as AppCompatActivity, Observer{ programs ->
             var programsArray = arrayOfNulls<String>(programs.size)
             for((i, program) in programs.withIndex())
             {
-                programsArray[i]=program.name
+                if(i==0)
+                {
+                    programsArray[i]=getString(R.string.not_selected_program)
+                }
+                else{
+                    programsArray[i]=program.name
+                }
+
             }
             programSpinner.adapter = ArrayAdapter<String>(context!!,
                 android.R.layout.simple_expandable_list_item_1,programsArray)
@@ -57,25 +69,30 @@ class ProgramBatchFragment: Fragment() {
                     id: Long
                 )
                 {
-                    selectedProgram= programs[position]
-                    batchesViewModel.getProgramBatches(selectedProgram!!.id)
+                    if(position!=0)
+                    {
+                        selectedProgram= programs[position]
+                        batchesViewModel.getProgramBatches(selectedProgram!!.id)
+                    }
+
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {
                     //result.text=R.string.not_selected_program.toString()
                 }
             }
-
-
-
-
         })
 
         batchesViewModel._batches.observe(activity as AppCompatActivity, Observer{ batches ->
             var batchesArray = arrayOfNulls<String>(batches.size)
             for((i, batch) in batches.withIndex())
             {
-                batchesArray[i]=batch.name
+                if(i==0)
+                {
+                    batchesArray[i]= getString(R.string.not_selected_batch)
+                }else {
+                    batchesArray[i] = batch.name
+                }
             }
             batchSpinner.adapter = ArrayAdapter<String>(context!!,
                 android.R.layout.simple_expandable_list_item_1,batchesArray)
@@ -87,7 +104,11 @@ class ProgramBatchFragment: Fragment() {
                     id: Long
                 )
                 {
-                    selectedBatch =batches[position]
+                    if(position!=0)
+                    {
+                        selectedBatch =batches[position]
+                    }
+
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {
